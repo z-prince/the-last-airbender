f2a4086593e09af96155829144e7beb9b123319d
Zachary P <zachary.s.prince@outlook.com>
2022-07-22 11:51:48 -0400

    Update service to search for nation specific characters

--- app/services/avatars_service.rb
+++ app/services/avatars_service.rb
@@ -1,7 +1,7 @@
 class AvatarsService
   def self.get_url(url, keyword = nil)
     conn = Faraday.new(url: 'https://last-airbender-api.herokuapp.com/') do |faraday|
-      faraday.params[:query] = keyword unless keyword.nil?
+      faraday.params[:affiliation] = keyword unless keyword.nil?
     end
 
     response = conn.get(url)
@@ -9,7 +9,7 @@ class AvatarsService
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def self.get_all_characters
-    get_url('api/v1/characters')
+  def self.get_all_nation_members(keyword)
+    get_url('api/v1/characters', keyword)
   end
 end
