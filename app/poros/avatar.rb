c3c7e523b9c3888c9b0a8edab0ab75e9469815df
Zachary P <zachary.s.prince@outlook.com>
2022-07-22 12:44:28 -0400

    Add enemies attribute after oversight

--- app/poros/avatar.rb
+++ app/poros/avatar.rb
@@ -2,12 +2,14 @@ class Avatar
   attr_reader :name,
               :photo,
               :allies,
-              :affiliations
+              :affiliations,
+              :enemies
 
   def initialize(data)
     @name = data[:name]
     @photo = data[:photoUrl] unless data[:photoUrl].nil?
     @allies = data[:allies]
     @affiliations = data[:affiliation] unless data[:affiliation].nil?
+    @enemies = data[:enemies] unless data[:enemies].nil?
   end
 end
